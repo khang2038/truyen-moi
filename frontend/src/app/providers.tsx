d4f9d02703bcd2ea2e36b6a07b59bb0c3025c578
khang2038 <trankhang9c2003@gmail.com>
2026-01-01 01:09:53 +0700

    fix: cache

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -72,7 +72,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
       new QueryClient({
         defaultOptions: {
           queries: {
-            staleTime: 60 * 1000,
+            staleTime: 10 * 1000,
             refetchOnWindowFocus: false,
           },
         },
